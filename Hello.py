ad797dc6c9c2798ad99060b480087ed6bc3b9611
pravalika2542 <124230743+pravalika2542@users.noreply.github.com>
2024-02-08 09:04:52 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,3 +1,5 @@
 import streamlit as st
 st.set_page_config(page_title='cartoonify')        
-st.title("Cartoon effect on image")     
+st.title("Cartoon effect on image")   
+
+uploaded_file = st.file_uploader("Choose an image...", type=["png"])
