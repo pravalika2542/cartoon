ca801ed4501391a9b6e40e088ff579e71d3ff6e0
pravalika2542 <124230743+pravalika2542@users.noreply.github.com>
2024-02-08 09:34:17 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -13,3 +13,8 @@ uploaded_file = st.file_uploader("Choose an image...", type=["png"])
 #displaying the uploaded image
 if uploaded_file is not None:
     st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
+    
+    # Convert the file to an opencv image
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    image = cv2.imdecode(file_bytes, 1)
+    st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
