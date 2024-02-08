923a5d62bf88286df17a8a3530df46876ee3f09b
pravalika2542 <124230743+pravalika2542@users.noreply.github.com>
2024-02-08 09:08:13 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -3,3 +3,7 @@ st.set_page_config(page_title='cartoonify')
 st.title("Cartoon effect on image")   
 
 uploaded_file = st.file_uploader("Choose an image...", type=["png"])
+
+if uploaded_file is not None:
+    # Display the image
+    st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
