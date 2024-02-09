fd25cc8d37b60c3bc731bc2a49ba8e9f8f6e44f1
pravalika2542 <124230743+pravalika2542@users.noreply.github.com>
2024-02-09 09:59:49 +0530

    Update cartoon.py

--- cartoon.py
+++ cartoon.py
@@ -1,1 +1,21 @@
 
+#importing the package
+import streamlit as st
+import cv2
+import numpy as np
+
+#setting the pagetitle
+st.set_page_config(page_title='cartoonify')   
+#setting the heading of the page
+st.title("Cartoon effect on image")   
+
+#uploading an user input(img)
+uploaded_file = st.file_uploader("Choose an image...", type=["png"])
+#displaying the uploaded image
+if uploaded_file is not None:
+    st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
+    
+    # Convert the file to an opencv image
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    image = cv2.imdecode(file_bytes, 1)
+    
